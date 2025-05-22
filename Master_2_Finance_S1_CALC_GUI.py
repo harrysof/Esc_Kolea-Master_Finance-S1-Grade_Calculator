c7ca92752bdb5e616caab77171e5da607c571f80
Sofiane Belkacem Nacer <73562755+harrysof@users.noreply.github.com>
2025-05-23 00:01:41 +0100

    Update Master_2_Finance_S1_CALC_GUI.py

--- Master_2_Finance_S1_CALC_GUI.py
+++ Master_2_Finance_S1_CALC_GUI.py
@@ -10,7 +10,7 @@ st.markdown("""
     <style>
     .main-title {
         font-size: 2.5rem;
-        color: #ff812f;
+        color: #FFCDAC;
         text-align: center;
         padding: 1.5rem 0;
         background: #0e1118;
